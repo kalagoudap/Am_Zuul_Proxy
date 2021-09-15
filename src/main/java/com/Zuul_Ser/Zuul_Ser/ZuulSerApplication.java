a6b22a2d111479f4b6acfe680f223d94800d29f4
kalagoudap <43758241+kalagoudap@users.noreply.github.com>
2021-09-15 17:22:43 +0530

    Add files via upload

---
+++ src/main/java/com/Zuul_Ser/Zuul_Ser/ZuulSerApplication.java
@@ -0,0 +1,13 @@
+package com.Zuul_Ser.Zuul_Ser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulSerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulSerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Zuul_Ser/Zuul_Ser/ZuulSerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Zuul_Ser.Zuul_Ser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulSerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
